e515ff7731e1bab2dfc8759fccdd5b6b7f270897
Casey Locker <casey@caseylocker.com>
2022-02-09 16:21:12 -0600

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,101 +1,152 @@
 import React from "react";
 import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
+//import github from "../img/github-icon.svg";
 import logo from "../img/logo.svg";
 
 const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: false,
-      navBarActiveClass: "",
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			active: false,
+			navBarActiveClass: "",
+		};
+	}
 
-  toggleHamburger() {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: "is-active",
-            })
-          : this.setState({
-              navBarActiveClass: "",
-            });
-      }
-    );
-  }
+	toggleHamburger() {
+		// toggle the active boolean in the state
+		this.setState(
+			{
+				active: !this.state.active,
+			},
+			// after state has been updated,
+			() => {
+				// set the class in state for the navbar accordingly
+				this.state.active
+					? this.setState({
+							navBarActiveClass: "is-active",
+					  })
+					: this.setState({
+							navBarActiveClass: "",
+					  });
+			}
+		);
+	}
 
-  render() {
-    return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              role="menuitem"
-              tabIndex={0}
-              onKeyPress={() => this.toggleHamburger()}
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+	render() {
+		return (
+			<nav className="navbar" role="navigation" aria-label="main-navigation">
+				<div className="container">
+					<div className="navbar-brand">
+						<Link to="/" className="navbar-item" title="Logo">
+							<img src={logo} alt="Kaldi" style={{ width: "321px" }} />
+						</Link>
+						{/* Hamburger menu */}
+						<div
+							className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+							data-target="navMenu"
+							role="menuitem"
+							tabIndex={0}
+							onKeyPress={() => this.toggleHamburger()}
+							onClick={() => this.toggleHamburger()}
+						>
+							<span />
+							<span />
+							<span />
+						</div>
+					</div>
+					<div id="navMenu" className={`navbar-menu ${this.state.navBarActiveClass}`}>
+						<div className="navbar-end has-text-centered">
+							<Link className="navbar-item" to="/about">
+								Getting Started
+							</Link>
+							<Link className="navbar-item" to="/blog">
+								Fundamentals
+							</Link>
+							<div className="navbar-item has-dropdown is-hoverable">
+								<Link className="navbar-link is-arrowless" to="/products">
+									Components
+								</Link>
+								<div className="navbar-dropdown">
+									<Link className="navbar-item" to="/">
+										Avatar
+									</Link>
+									<Link className="navbar-item" to="/">
+										Breadcrumbs
+									</Link>
+									<Link className="navbar-item" to="/">
+										Button
+									</Link>
+									<Link className="navbar-item" to="/">
+										Checkbox
+									</Link>
+									<Link className="navbar-item" to="/">
+										Divider
+									</Link>
+									<Link className="navbar-item" to="/">
+										Link
+									</Link>
+									<Link className="navbar-item" to="/">
+										List
+									</Link>
+									<Link className="navbar-item" to="/">
+										Loader
+									</Link>
+									<Link className="navbar-item" to="/">
+										Logo
+									</Link>
+									<Link className="navbar-item" to="/">
+										Notification
+									</Link>
+									<Link className="navbar-item" to="/">
+										Pagination
+									</Link>
+									<Link className="navbar-item" to="/">
+										Password Input
+									</Link>
+									<Link className="navbar-item" to="/">
+										Progress Indicator
+									</Link>
+									<Link className="navbar-item" to="/">
+										Radio Button
+									</Link>
+									<Link className="navbar-item" to="/">
+										Select
+									</Link>
+									<Link className="navbar-item" to="/">
+										Tabs
+									</Link>
+									<Link className="navbar-item" to="/">
+										Tag
+									</Link>
+									<Link className="navbar-item" to="/">
+										Text Area
+									</Link>
+									<Link className="navbar-item" to="/">
+										Text Input
+									</Link>
+									<Link className="navbar-item" to="/">
+										Toggle
+									</Link>
+									<Link className="navbar-item" to="/">
+										Tooltip
+									</Link>
+								</div>
+							</div>
+							<Link className="navbar-item" to="/contact">
+								Widgets
+							</Link>
+							<Link className="navbar-item" to="/contact">
+								UI Shell
+							</Link>
+							<Link className="navbar-item" to="/about">
+								Page Templates
+							</Link>
+						</div>
+					</div>
+				</div>
+			</nav>
+		);
+	}
 };
 
 export default Navbar;
--- src/pages/index.md
+++ src/pages/index.md
@@ -1,70 +1,42 @@
 ---
 templateKey: index-page
-title: Great coffee with a conscience
-image: /img/home-jumbotron.jpg
-heading: Great coffee with a conscience
-subheading: Support sustainable farming while enjoying a cup
-mainpitch:
-  title: Why Kaldi
-  description: >
-    Kaldi is the coffee store for everyone who believes that great coffee
-    shouldn't just taste good, it should do good too. We source all of our beans
-    directly from small scale sustainable farmers and make sure part of the
-    profits are reinvested in their communities.
+title: Homepage Title
+image: /img/homepage-hero.png
+heading: H1 Page Heading
+subheading: Homepage hero subheading to really drive it home
 description: >-
-  Kaldi is the ultimate spot for coffee lovers who want to learn about their
-  java’s origin and support the farmers that grew it. We take coffee production,
-  roasting and brewing seriously and we’re glad to pass that knowledge to
-  anyone.
+    Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Quis vel eros donec ac odio tempor. Sed vulputate mi sit amet mauris commodo quis. Duis at tellus at urna condimentum mattis. Purus in mollis nunc sed id semper risus in. Est ante in nibh mauris cursus mattis molestie a iaculis. Curabitur vitae nunc sed velit dignissim sodales ut eu. Sit amet luctus venenatis lectus magna fringilla urna porttitor rhoncus. Nulla pellentesque.
 intro:
-  blurbs:
-    - image: /img/coffee.png
-      text: >
-        We sell green and roasted coffee beans that are sourced directly from
-        independent farmers and farm cooperatives. We’re proud to offer a
-        variety of coffee beans grown with great care for the environment and
-        local communities. Check our post or contact us directly for current
-        availability.
-    - image: /img/coffee-gear.png
-      text: >
-        We offer a small, but carefully curated selection of brewing gear and
-        tools for every taste and experience level. No matter if you roast your
-        own beans or just bought your first french press, you’ll find a gadget
-        to fall in love with in our shop.
-    - image: /img/tutorials.png
-      text: >
-        Love a great cup of coffee, but never knew how to make one? Bought a
-        fancy new Chemex but have no clue how to use it? Don't worry, we’re here
-        to help. You can schedule a custom 1-on-1 consultation with our baristas
-        to learn anything you want to know about coffee roasting and brewing.
-        Email us or call the store for details.
-    - image: /img/meeting-space.png
-      text: >
-        We believe that good coffee has the power to bring people together.
-        That’s why we decided to turn a corner of our shop into a cozy meeting
-        space where you can hang out with fellow coffee lovers and learn about
-        coffee making techniques. All of the artwork on display there is for
-        sale. The full price you pay goes to the artist.
-  heading: What we offer
-  description: >
-    Kaldi is the ultimate spot for coffee lovers who want to learn about their
-    java’s origin and support the farmers that grew it. We take coffee
-    production, roasting and brewing seriously and we’re glad to pass that
-    knowledge to anyone. This is an edit via identity...
+    blurbs:
+        - title: H2 Sub Heading Goes Here
+          text: >
+              Sed sed risus pretium quam vulputate dignissim suspendisse in. Ullamcorper eget nulla facilisi etiam dignissim diam. Turpis egestas sed tempus urna et pharetra. Massa id neque aliquam vestibulum morbi blandit cursus risus. Consequat mauris nunc congue nisi vitae suscipit tellus mauris. Dui nunc mattis enim ut. Mauris ultrices.
+        - title: H2 Sub Heading Goes Here
+          text: >
+              Auctor elit sed vulputate mi sit. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Posuere sollicitudin aliquam ultrices sagittis orci a. Massa tincidunt dui ut ornare lectus sit. Nullam eget felis eget nunc lobortis mattis aliquam. Id eu nisl nunc mi ipsum. Malesuada fames ac turpis egestas maecenas pharetra convallis posuere morbi. Est lorem ipsum dolor sit amet consectetur adipiscing elit. Enim diam vulputate ut pharetra. Morbi tristique senectus et netus et malesuada fames ac turpis. Contact us at: [productdesign@hidglobal.com](mailto:productdesign@hidglobal.com)
+        - title: H2 Sub Heading Goes Here
+          text: >
+              Ac tortor vitae purus faucibus ornare suspendisse sed. Et netus et malesuada fames. Felis imperdiet proin fermentum leo vel orci porta. Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Id porta nibh venenatis cras. Ut porttitor leo a diam sollicitudin tempor. Viverra suspendisse potenti nullam.
+    heading: What we offer
+    description: >
+        Kaldi is the ultimate spot for coffee lovers who want to learn about their
+        java’s origin and support the farmers that grew it. We take coffee
+        production, roasting and brewing seriously and we’re glad to pass that
+        knowledge to anyone. This is an edit via identity...
 main:
-  heading: Great coffee with no compromises
-  description: >
-    We hold our coffee to the highest standards from the shrub to the cup.
-    That’s why we’re meticulous and transparent about each step of the coffee’s
-    journey. We personally visit each farm to make sure the conditions are
-    optimal for the plants, farmers and the local environment.
-  image1:
-    alt: A close-up of a paper filter filled with ground coffee
-    image: /img/products-grid3.jpg
-  image2:
-    alt: A green cup of a coffee on a wooden table
-    image: /img/products-grid2.jpg
-  image3:
-    alt: Coffee beans
-    image: /img/products-grid1.jpg
+    heading: Great coffee with no compromises
+    description: >
+        We hold our coffee to the highest standards from the shrub to the cup.
+        That’s why we’re meticulous and transparent about each step of the coffee’s
+        journey. We personally visit each farm to make sure the conditions are
+        optimal for the plants, farmers and the local environment.
+    image1:
+        alt: A close-up of a paper filter filled with ground coffee
+        image: /img/products-grid3.jpg
+    image2:
+        alt: A green cup of a coffee on a wooden table
+        image: /img/products-grid2.jpg
+    image3:
+        alt: Coffee beans
+        image: /img/products-grid1.jpg
 ---
